dd5e84342b27895d16cdbe0b9d5677d194978508
Nguyen Huu Dang <nguyenhuudan4811@gmail.com>
2024-11-25 22:32:41 +0700

    tạo from đăng nhập

--- FormLogin.cs
+++ FormLogin.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace windowforms_sqlsever
 {
     public partial class FormLogin : Form
     {
+        public bool IsAuthenticated { get; private set; } = false; // Trạng thái đăng nhập
+
         public FormLogin()
         {
             InitializeComponent();
+            button1.Click += button1_Click; // Gán sự kiện cho nút "Log in"
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text; // Lấy giá trị từ ô tài khoản
+            string password = textBox2.Text; // Lấy giá trị từ ô mật khẩu
+
+            // Kiểm tra thông tin đăng nhập (ở đây sử dụng dữ liệu cố định)
+            if (username == "admin" && password == "12345")
+            {
+                IsAuthenticated = true; // Đăng nhập thành công
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close(); // Đóng form đăng nhập
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace windowforms_sqlsever
 {
     internal static class Program
     {
-       
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            // Hiển thị FormLogin
+            FormLogin loginForm = new FormLogin();
+            Application.Run(loginForm);
+
+            // Nếu đăng nhập thành công, mở Form1
+            if (loginForm.IsAuthenticated)
+            {
+                Application.Run(new Form1());
+            }
         }
     }
 }
